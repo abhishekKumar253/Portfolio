026fe0f8a3080dad9df91fc89ad23acc15295be5
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2024-03-25 15:57:23 +0530

    Home

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -45,7 +45,8 @@ const Home2 = () => {
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> 
+                 React.js and Next.js</b>
               </i>
               .
             </p>
